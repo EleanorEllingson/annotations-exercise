19be844a8734cfcb5c8a2c27fc0812b1402fdef2
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-12-17 19:35:25 -0600

    easy fix finished

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
 
@@ -8,5 +10,7 @@ public class Main {
         for (String book: author.getBooks()) {
             System.out.println(book);
         }
+
     }
+
 }
